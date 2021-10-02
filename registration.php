fd9991444a591c85998b8ed4c4b12ff1bedc5e20
Monish-Trivedi <monish.trivedi@krishtechnolabs.com>
2021-10-02 10:57:24 +0530

    Import CSv data module

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    Ktpl_Import
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Ktpl_Import',
+    __DIR__
+);
